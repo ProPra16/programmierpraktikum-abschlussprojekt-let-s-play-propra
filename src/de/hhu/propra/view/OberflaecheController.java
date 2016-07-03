8c07e73d85132d168cdc424b8995628091efbdb0
yutak001 <Yumiko.Takahashi@hhu.de>
2016-07-03 12:24:07 +0200

    Code- und Test-Testfeld skalierbar; Wechselbuttons hinzugefuegt; Setter für Konsolentext und Disabler für Code und Test hinzugefügt (Interface als API)

--- src/de/hhu/propra/view/OberflaecheController.java
+++ src/de/hhu/propra/view/OberflaecheController.java
@@ -6,12 +6,17 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 
-public class OberflaecheController {
+public class OberflaecheController implements OberflaecheControllerInterface{
+	public static boolean wechsel = false;
+	
 	@FXML
 	private TextArea testTextArea;
 	
 	@FXML
 	private Button testPruefen;
+	
+	@FXML
+	private Button testPruefenUndWechsel;
 	
 	@FXML
 	private Button testLeeren;
@@ -21,6 +26,9 @@ public class OberflaecheController {
 	
 	@FXML
 	private Button codePruefen;
+	
+	@FXML
+	private Button codePruefenUndWechsel;
 	
 	@FXML
 	private Button codeLeeren;
@@ -33,45 +41,74 @@ public class OberflaecheController {
 	
 	@FXML
 	protected void handleTestPruefen(){
-		testTextArea.setEditable(false);
-		testTextArea.setStyle("-fx-control-inner-background: #555555");
-		testPruefen.setDisable(true);
-		testLeeren.setDisable(true);
-		
-		codeTextArea.setEditable(true);
-		codeTextArea.setStyle("");
-		codePruefen.setDisable(false);
-		codeLeeren.setDisable(false);
+		wechsel = false;
 		for (int i=0;i<5;i++) {
 			fehlgeschlageneTests.getItems().add(i+"");
 		}
-		System.out.println("Test prüfen");
+	}
+	
+	@FXML
+	protected void handleTestPruefenUndWechsel(){
+		disableTestArea();
+		wechsel = true;
+		for (int i=0;i<5;i++) {
+			fehlgeschlageneTests.getItems().add(i+"");
+		}
 	}
 	
 	@FXML
 	protected void handleTestLeeren(){
 		testTextArea.setText("");
-		System.out.println("Test geleert");
 	}
 	
 	@FXML
 	protected void handleCodeLeeren(){
 		codeTextArea.setText("");
-		System.out.println("Code geleert");
 	}
 	
 	@FXML
 	protected void handleCodePruefen(){
-		//testTextArea.setEditable(true);
-		//testTextArea.setStyle("");
-		//testPruefen.setDisable(false);
-		//testLeeren.setDisable(false);
-		
-		//codeTextArea.setEditable(false);
-		//codeTextArea.setStyle("-fx-control-inner-background: #555555");
-		//codePruefen.setDisable(true);
-		//codeLeeren.setDisable(true);
-		
+		wechsel = false;
 		CodeTester.testCode(codeTextArea.getText());
 	}
+	
+	@FXML
+	protected void handleCodePruefenUndWechsel(){
+		wechsel = true;
+		CodeTester.testCode(codeTextArea.getText());
+	}
+
+	public void disableCodeArea() {
+		testTextArea.setEditable(true);
+		testTextArea.setStyle("");
+		testPruefen.setDisable(false);
+		testPruefenUndWechsel.setDisable(false);
+		testLeeren.setDisable(false);
+		
+		codeTextArea.setEditable(false);
+		codeTextArea.setStyle("-fx-control-inner-background: #555555");
+		codePruefen.setDisable(true);
+		codePruefenUndWechsel.setDisable(true);
+		codeLeeren.setDisable(true);
+	}
+
+	public void disableTestArea() {
+		testTextArea.setEditable(false);
+		testTextArea.setStyle("-fx-control-inner-background: #555555");
+		testPruefen.setDisable(true);
+		testPruefenUndWechsel.setDisable(true);
+		testLeeren.setDisable(true);
+		
+		codeTextArea.setEditable(true);
+		codeTextArea.setStyle("");
+		codePruefen.setDisable(false);
+		codePruefenUndWechsel.setDisable(false);
+		codeLeeren.setDisable(false);
+	}
+
+	public void appendKonsoleText(String message) {
+		konsoleTextArea.appendText(message);
+	}
+	
+	
 }
