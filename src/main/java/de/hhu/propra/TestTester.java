939c4f0c525ec2e6328cec287e676b4a24e2d848
ViktorKoenigsmann <Viktor.Koenigsmann@uni-duesseldorf.de>
2016-07-12 03:21:58 +0200

    Test Tester überarbeitet, sollte jetzt funktionieren, muss nur in die GUI implementiert werden

--- src/main/java/de/hhu/propra/TestTester.java
+++ src/main/java/de/hhu/propra/TestTester.java
@@ -1,5 +1,6 @@
 package de.hhu.propra;
 
+import javafx.beans.property.SimpleStringProperty;
 import vk.core.api.CompilationUnit;
 import vk.core.api.CompileError;
 import vk.core.api.CompilerFactory;
@@ -9,54 +10,66 @@ import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 /**
  * Created by Viktor on 06.07.2016.
  */
 import vk.core.api.*;
-public class TestTester {
-  private static int TestAnzahl=0;
-  private List<Test> Tests = new ArrayList<Test>();
+public class TestTester extends SimpleStringProperty{
+  private String code;
   private static JavaStringCompiler Testcompiler;
-  private static String testname;
-  public static String TesteTest(String code,int TestAnzahl, String tabname) {
+  private static Tracker tracker;
+  private String dateiname;
+  private Collection<TestFailure> notWorking;
 
+  public String TestTester(String derCode) {
+    this.code=derCode;
     String ergebnis="";
-    for (int TestNr = 0; TestNr <= TestAnzahl; TestNr++) {
       //Tests.add(TestNr, new Test(DateiScanner));
 
-      testname = tabname;
-      CompilationUnit aktuellerTest = new CompilationUnit(testname, code, true);
-      Testcompiler = CompilerFactory.getCompiler(aktuellerTest);
+    CompilationUnit testUnit = new CompilationUnit("Test", code, true);
 
-      try {
+    Testcompiler = CompilerFactory.getCompiler(testUnit);
+
+    notWorking.addAll(Testcompiler.getTestResult().getTestFailures());
+    Object[] meinArray = notWorking.toArray();
+
+
+    try {
         Testcompiler.compileAndRunTests();
       } catch (Exception e) {
-        return ("Fehler beim Testausführen!" + e.toString());
+        return ("Fehler beim Testausfuehren!" + e.toString());
       }
 
       if (Testcompiler.getCompilerResult().hasCompileErrors()) {
-        return fehlerString(aktuellerTest,TestNr);
+        return fehlerString(testUnit);
       }
 
       try {
-        FileWriter writer = new FileWriter("temp/" + testname + ".java");
+        FileWriter writer = new FileWriter(Main.getCorrectPath() + "/aufgaben/Test.txt");
         writer.write(code);
         writer.close();
       } catch (Exception e) {
 
       }
       ergebnis = "Tests werden durchgeführt...";
-
-    }
     return (ergebnis);
   }
 
-  private static String fehlerString(CompilationUnit dieserTest,int TestNr){
+  private static String Rueckgabe(CompilationUnit dieserTest){
+    String rueckgabeString="";
+    rueckgabeString+="Fehlgeschlagene Tests: "+Testcompiler.getTestResult().getNumberOfFailedTests();
+    rueckgabeString+="\n";
+    rueckgabeString+="Erfolgreiche Tests: "+Testcompiler.getTestResult().getNumberOfSuccessfulTests();
+    rueckgabeString+="\n";
+    rueckgabeString+="Ignorierte Tests: "+Testcompiler.getTestResult().getNumberOfIgnoredTests();
+    return rueckgabeString;
+  }
+
+  private static String fehlerString(CompilationUnit dieserTest){
     String fehler = "";
-    int TestNummer=TestNr+1;
-    fehler = "Test "+TestNummer+" enthält Fehler";
     for (CompileError error : Testcompiler.getCompilerResult().getCompilerErrorsForCompilationUnit(dieserTest)){
       fehler += "\nin Zeile " + error.getLineNumber() + ": " + error.getMessage() + ": \n";
       fehler += error.getCodeLineContainingTheError() + "\n";
