68c70d93a0c3510cbc96c4b93a776684d4476c2d
DaveThaGee <d.gantschnigg@yahoo.de>
2016-07-12 16:41:35 +0200

    Einstellungsmenü und Gebrauchsanweisung implementiert

--- src/main/java/de/hhu/propra/view/HauptfensterController.java
+++ src/main/java/de/hhu/propra/view/HauptfensterController.java
@@ -6,11 +6,10 @@ import de.hhu.propra.model.Aufgabe;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.Menu;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
+
+import java.util.Optional;
 
 /**
  * Created by FreddyG on 06.07.16.
@@ -99,6 +98,51 @@ public class HauptfensterController {
             }
         });
         tempmenu.getItems().add(temp);
+    }
+
+    @FXML
+    public void handleMenueEinstellungen() {
+        TextField eingabe = new TextField("");
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Einstellungen");
+        alert.setHeaderText("Bitte legen Sie die Länge der Babysteps in Sekunden fest.");
+        ButtonType speichern = new ButtonType("Speichern");
+        ButtonType abbrechen = new ButtonType("Abbrechen");
+        alert.getButtonTypes().setAll(speichern, abbrechen);
+        alert.getDialogPane().setContent(eingabe);
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == speichern){
+            try {
+                int dauer = Integer.parseInt(eingabe.getText());
+                Alert confirmation = new Alert(Alert.AlertType.CONFIRMATION);
+                confirmation.setTitle("Achtung");
+                confirmation.setHeaderText("Ihre Eingabe wurde gespeichert.");
+                confirmation.showAndWait();
+                System.out.println(dauer);
+                OberflaecheController.start = dauer;
+                System.out.println(OberflaecheController.start); //Wie soll die Wertübergabe erfolgen?
+            }
+            catch (NumberFormatException dauer){
+                Alert warning = new Alert(Alert.AlertType.WARNING);
+                warning.setTitle("Warnung");
+                warning.setHeaderText("Ungültige Eingabe!");
+                warning.setContentText("Ihre Eingabe ist ungültig oder leer.");
+                warning.showAndWait();
+                alert.close();
+                handleMenueEinstellungen();
+            }
+        } else {}
+
+    }
+
+    @FXML
+    public void handleMenueGebrauchsanweisung() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Gebrauchsanweisung");
+        alert.setHeaderText(null);
+        alert.setResizable(true);
+        alert.getDialogPane().setContent(new TextArea("Hier wird später die Gebrauchsanweisung angezeigt."));
+        alert.showAndWait();
     }
 
     public void setMain(Main main){
